3b649fd3997302411c1de8a538daf20b13145815
Riad Developer <147613731+RiadDeveloper@users.noreply.github.com>
2024-04-15 02:27:26 +0600

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,7 +2,7 @@ const { Colors } = require("discord.js");
 
 const defaultSettings = {
   TOKEN: process.env.TOKEN || "BOT_TOKEN",
-  PREFIX: process.env.PREFIX || "!",
+  PREFIX: process.env.PREFIX || "/",
   MONGO_URL: process.env.MONGO_URL || "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
 };
 
